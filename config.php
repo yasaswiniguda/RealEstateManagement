af163907fd8528fc0355cf6f42c96955319ff28e
yasaswiniguda <120549822+yasaswiniguda@users.noreply.github.com>
2023-12-11 23:15:54 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,6 @@
 <?php
-$servername="yashu.mysql.database.azure.com"
-$username="yashu"
-$password="Sravs@24032003"
-$databasename="realestatephp"
-$con = mysqli_connect($servername,$username,$password,$databasename);
+
+$con = mysqli_connect("yashu.mysql.database.azure.com","yashu","Sravs@24032003","realestatephp");
 	if (mysqli_connect_errno())
 	{
 		echo "Failed to connect to MySQL: " . mysqli_connect_error();
